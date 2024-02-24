5d70e8441e3be85ac3394de654b5c1437c4b5665
rinklepoonia <rinklepoonia889@gmail.com>
2024-02-24 16:52:02 +0530

    FAQ-accordian

---
+++ assets/js/style.js
@@ -0,0 +1,70 @@
+const menuIcon = document.querySelector(".menuIcon")
+const menulist = document.querySelector(".menulist")
+
+
+menuIcon.addEventListener("click", function () {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overFlowHidden")
+    document.querySelector(".menuIcon").classList.toggle("cross")
+})
+
+// ===============tabes============
+function navtabs(tabs) {
+    const tabshow = document.querySelectorAll('.tabshow')
+    const tabbtn = document.querySelectorAll('.tabs button')
+    tabshow.forEach(content => {
+        content.style.display = 'none'
+    })
+    const selecttoshow = document.getElementById(tabs)
+    selecttoshow.style.display = 'block'
+}
+navtabs('tab1')
+// =========accordion=========
+const accordionHead = document.querySelectorAll(".accordionHead");
+accordionHead.forEach((e) => {
+    e.addEventListener("click", () => {
+        const activeAccordion = document.querySelector(".active")
+        e.parentElement.classList.toggle("active")
+        activeAccordion && activeAccordion.classList.remove("active")
+    })
+})
+
+// ============slider=============
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});

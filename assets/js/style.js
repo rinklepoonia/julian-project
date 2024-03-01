bc02c40abf00a8ee7aa4017ce4a15bdaedbfc6cd
rinklepoonia <rinklepoonia889@gmail.com>
2024-03-01 22:04:19 +0530

    timrlines

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,7 +1,6 @@
 const menuIcon = document.querySelector(".menuIcon")
 const menulist = document.querySelector(".menulist")
 
-
 menuIcon.addEventListener("click", function () {
     menulist.classList.toggle("show");
     document.body.classList.toggle("overFlowHidden")

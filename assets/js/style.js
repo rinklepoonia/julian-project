e51e0375766b1dccb60faeab9a48d8eca0d309de
rinklepoonia <rinklepoonia889@gmail.com>
2024-03-03 18:34:45 +0530

    page-done

--- assets/js/style.js
+++ assets/js/style.js
@@ -47,6 +47,21 @@ $(document).ready(function () {
 });
 navtabs_history('tabhistory1')
 
+// ===========back-to-top===========
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
+
 // ============slider=============
 $('.responsive').slick({
     dots: false,
